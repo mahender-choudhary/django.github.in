8d96afbf352a701a9f5ede3c69b53fa8b9e4fb56
Mahendra Choudhary <myios7726@gmail.com>
2023-01-02 21:11:11 +0530

    1.4

--- firstApp/firstApp/views.py
+++ firstApp/firstApp/views.py
@@ -4,5 +4,18 @@ def index(request):
     return render(request, 'index.html')
 
 
-def about(request):
-    return HttpResponse("about Mahendra Choudhary")
+def analyze(request):
+    var1 = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    print(removepunc)
+    print(var1)
+    if removepunc == 'on':
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in var1:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Remove Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse("Error")
