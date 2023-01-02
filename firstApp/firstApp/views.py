53ae0281f8fe02ac06dbb3be3cc668e9790f6304
Mahendra Choudhary <myios7726@gmail.com>
2023-01-02 15:05:02 +0530

    1.3

--- firstApp/firstApp/views.py
+++ firstApp/firstApp/views.py
@@ -1,6 +1,8 @@
 from django.http import  HttpResponse
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, 'index.html')
+
 
 def about(request):
     return HttpResponse("about Mahendra Choudhary")
